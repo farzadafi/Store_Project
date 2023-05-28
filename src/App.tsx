ec063c01dd347c89bfb0b4505a35f84eb85e099b
farzadafi <farzadafi40@gmail.com>
2023-05-28 16:47:55 +0330

    add new rout to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import {HomePage} from "@/pages";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path={"/"} Component={HomePage}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
