e24273e936a44bfe157266c4acd5231a3d4517f3
farzadafi <farzadafi40@gmail.com>
2023-05-29 15:51:00 +0330

    add some route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import {HomePage} from "@/pages";
+import {Basket, HomePage, InventoryAndPrices} from "@/pages";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Container} from "@/containers";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={"/"} Component={HomePage}></Route>
-      </Routes>
-    </Router>
+    <Container>
+      <Router>
+        <Routes>
+          <Route path={"/"} Component={HomePage}></Route>
+          <Route path={"/inventory-and-prices"} Component={InventoryAndPrices}></Route>
+          <Route path={"/basket"} Component={Basket}></Route>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
