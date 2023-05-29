b8a54e1286cc147177e2a45ac6c83f009d84587c
farzadafi <farzadafi40@gmail.com>
2023-05-29 13:45:20 +0330

    create InventoryAndPrices component

---
+++ src/pages/manager/inventoryAndPrices/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InventoryAndPrices = () => {
+  return (
+    <div>
+      <p>inventory and prices manager panel</p>
+    </div>
+  );
+};
+
+export default InventoryAndPrices;
