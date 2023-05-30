e29e41e8947fdfcaeef7b0700a6260e3d9853eba
farzadafi <farzadafi40@gmail.com>
2023-05-30 07:42:33 +0330

    remove @ResponseStatus

--- online-store/src/main/java/com/farzadafi/online_store/exception/DuplicateException.java
+++ online-store/src/main/java/com/farzadafi/online_store/exception/DuplicateException.java
@@ -1,9 +1,5 @@
 package com.farzadafi.online_store.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT,reason = "already exists")
 public class DuplicateException extends RuntimeException{
 
     public DuplicateException(String message) {
