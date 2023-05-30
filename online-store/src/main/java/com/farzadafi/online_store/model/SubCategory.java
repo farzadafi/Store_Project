7f4118170521e9103f42e7edebd01af53ce1268b
farzadafi <farzadafi40@gmail.com>
2023-05-30 14:25:32 +0330

    add new set for product also add a helper method

--- online-store/src/main/java/com/farzadafi/online_store/model/SubCategory.java
+++ online-store/src/main/java/com/farzadafi/online_store/model/SubCategory.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -18,4 +21,10 @@ public class SubCategory {
     private String name;
 
     private String categoryId;
+
+    private Set<Product> subCategories = new HashSet<>();
+
+    public void addProduct(Product product) {
+        this.subCategories.add(product);
+    }
 }
