ee194c92aa0101cee12d6317ea955e9d768df2c9
farzadafi <farzadafi40@gmail.com>
2023-05-30 14:06:49 +0330

    fix addSubCategory method

--- online-store/src/main/java/com/farzadafi/online_store/service/SubCategoryService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/SubCategoryService.java
@@ -5,19 +5,21 @@ import com.farzadafi.online_store.model.Category;
 import com.farzadafi.online_store.model.SubCategory;
 import com.farzadafi.online_store.repository.SubCategoryRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public record SubCategoryService(SubCategoryRepository subCategoryRepository,
                                  CategoryService categoryService) {
-
     public void addSubCategory(String categoryId, SubCategory subCategory) {
         Category category = categoryService.findById(categoryId);
-        if(subCategoryRepository.findByName(subCategory.getName()).block() != null)
+        if (subCategoryRepository.findByName(subCategory.getName()).block() != null)
             throw new DuplicateException("این نام از قبل موجود میباشید");
         subCategory.setId(UUID.randomUUID().toString());
-        subCategory.setCategory(category);
+        subCategory.setCategoryId(categoryId);
+        category.addSubCategory(subCategory);
+        categoryService.updateCategory(category);
         subCategoryRepository.save(subCategory).subscribe();
     }
 }
