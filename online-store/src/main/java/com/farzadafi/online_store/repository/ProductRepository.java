737d3e92110231cd961fd324c06541c53119ac2f
farzadafi <farzadafi40@gmail.com>
2023-05-30 15:37:17 +0330

    add findAllBySubCategoryId method

--- online-store/src/main/java/com/farzadafi/online_store/repository/ProductRepository.java
+++ online-store/src/main/java/com/farzadafi/online_store/repository/ProductRepository.java
@@ -3,8 +3,11 @@ package com.farzadafi.online_store.repository;
 import com.farzadafi.online_store.model.Category;
 import com.farzadafi.online_store.model.Product;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
     Mono<Category> findByName(String name);
+    Flux<Product> findAllBySubCategoryId(String subCategoryId);
+
 }
