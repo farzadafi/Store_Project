21ac01c327c7e81c3dc7657016189dede3980088
farzadafi <farzadafi40@gmail.com>
2023-05-30 09:26:55 +0330

    remove some unused import

---
+++ online-store/src/main/java/com/farzadafi/online_store/controller/SubCategoryController.java
@@ -0,0 +1,23 @@
+package com.farzadafi.online_store.controller;
+
+import com.farzadafi.online_store.dto.ReturnMessage;
+import com.farzadafi.online_store.dto.SubCategoryDto;
+import com.farzadafi.online_store.mapper.SubCategoryMapper;
+import com.farzadafi.online_store.model.SubCategory;
+import com.farzadafi.online_store.service.SubCategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sub-category")
+public record SubCategoryController(SubCategoryService subCategoryService) {
+
+    @PostMapping("/add")
+    public ResponseEntity<ReturnMessage> addCategory(@RequestBody SubCategoryDto subCategoryDto,
+                                                     @RequestParam String categoryId) {
+        SubCategory subCategory = SubCategoryMapper.INSTANCE.dtoToModel(subCategoryDto);
+        subCategoryService.addSubCategory(categoryId, subCategory);
+        return new ResponseEntity<>(new ReturnMessage("ok", 201), HttpStatus.CREATED);
+    }
+}
