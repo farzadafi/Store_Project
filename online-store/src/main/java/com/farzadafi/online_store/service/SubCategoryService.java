39cda1371449573a18fea178800a505ea94de855
farzadafi <farzadafi40@gmail.com>
2023-05-30 15:36:29 +0330

    add updateSuhCategory method

--- online-store/src/main/java/com/farzadafi/online_store/service/SubCategoryService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/SubCategoryService.java
@@ -1,11 +1,13 @@
 package com.farzadafi.online_store.service;
 
 import com.farzadafi.online_store.exception.DuplicateException;
+import com.farzadafi.online_store.exception.NotFoundException;
 import com.farzadafi.online_store.model.Category;
 import com.farzadafi.online_store.model.SubCategory;
 import com.farzadafi.online_store.repository.SubCategoryRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.*;
 
@@ -23,11 +25,22 @@ public record SubCategoryService(SubCategoryRepository subCategoryRepository,
         subCategoryRepository.save(subCategory).subscribe();
     }
 
+    public void updateSubCategory(SubCategory subCategory) {
+        subCategoryRepository.save(subCategory).subscribe();
+    }
+
     public List<SubCategory> findAllByCategoryId(String categoryId) {
         if (categoryService.findById(categoryId) != null) {
             Flux<SubCategory> allByCategoryId = subCategoryRepository.findAllByCategoryId(categoryId);
             return allByCategoryId.collectList().block();
         }
         return null;
+    }
+
+    public SubCategory findById(String id) {
+        var categoryMono = subCategoryRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(String.format("دسته بندی با این آیدی پیدا نشد %s", id))));
+        return categoryMono.block();
     }
 }
