5bae25012f22085d59d2ebf32c4a5fb4b31b9d90
farzadafi <farzadafi40@gmail.com>
2023-05-30 07:45:22 +0330

    remove lombok annotation

--- online-store/src/main/java/com/farzadafi/online_store/dto/CategoryDto.java
+++ online-store/src/main/java/com/farzadafi/online_store/dto/CategoryDto.java
@@ -1,11 +1,4 @@
 package com.farzadafi.online_store.dto;
 
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Builder
 public record CategoryDto(int id, String name) {
 }
