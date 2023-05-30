efa54825077c1afd33a2527b1621d76430ef4169
farzadafi <farzadafi40@gmail.com>
2023-05-30 09:25:53 +0330

    add findById method

--- online-store/src/main/java/com/farzadafi/online_store/service/CategoryService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.farzadafi.online_store.service;
 
 import com.farzadafi.online_store.exception.DuplicateException;
+import com.farzadafi.online_store.exception.NotFoundException;
 import com.farzadafi.online_store.model.Category;
 import com.farzadafi.online_store.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
 import java.util.UUID;
 
 @Service
@@ -11,8 +14,15 @@ public record CategoryService(CategoryRepository categoryRepository) {
 
     public void addCategory(Category category) {
         category.setId(UUID.randomUUID().toString());
-        if(categoryRepository.findByName(category.getName()).block() != null)
+        if (categoryRepository.findByName(category.getName()).block() != null)
             throw new DuplicateException("این نام از قبل موجود میباشید");
         categoryRepository.save(category).subscribe();
+    }
+
+    public Category findById(String id) {
+        var categoryMono = categoryRepository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException(String.format("دسته بندی با این آیدی پیدا نشد %s", id))));
+        return categoryMono.block();
     }
 }
