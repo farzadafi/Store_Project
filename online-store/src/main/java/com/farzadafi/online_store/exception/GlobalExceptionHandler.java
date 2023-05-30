2b24ee4110f9f9811a2bc68e58fb781c2f5a3ac5
farzadafi <farzadafi40@gmail.com>
2023-05-30 07:42:14 +0330

    create GlobalExceptionHandler class

---
+++ online-store/src/main/java/com/farzadafi/online_store/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.farzadafi.online_store.exception;
+
+import com.farzadafi.online_store.dto.ReturnMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<ReturnMessage> handleDuplicateException(DuplicateException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ReturnMessage(e.getMessage(), HttpStatus.CONFLICT.value()));
+    }
+}
