975676b7d05b3d6667a991f0e990030f2feea231
farzadafi <farzadafi40@gmail.com>
2023-05-30 07:45:45 +0330

    remove lombok annotation

--- online-store/src/main/java/com/farzadafi/online_store/dto/ReturnMessage.java
+++ online-store/src/main/java/com/farzadafi/online_store/dto/ReturnMessage.java
@@ -1,11 +1,4 @@
 package com.farzadafi.online_store.dto;
 
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Builder
 public record ReturnMessage(String message,int code) {
 }
