eacec372fb0d15cb63f0862c6cbdb81b56464ce1
farzadafi <farzadafi40@gmail.com>
2023-05-30 14:35:10 +0330

    create Product model

---
+++ online-store/src/main/java/com/farzadafi/online_store/model/Product.java
@@ -0,0 +1,35 @@
+package com.farzadafi.online_store.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.bson.types.Binary;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document
+public class Product {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private String name;
+
+    private Long Price;
+
+    private Integer quantity;
+
+    private String brand;
+
+    private String description;
+
+    private Integer rate;
+
+    private Binary image;
+
+    private String subCategoryId;
+}
