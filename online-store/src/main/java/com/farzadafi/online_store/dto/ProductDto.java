dae0e8758d6f97cd3659b2a6627e8ea52dad6df4
farzadafi <farzadafi40@gmail.com>
2023-05-30 15:01:15 +0330

    fix type of image to MultiPartFile

--- online-store/src/main/java/com/farzadafi/online_store/dto/ProductDto.java
+++ online-store/src/main/java/com/farzadafi/online_store/dto/ProductDto.java
@@ -1,14 +1,13 @@
 package com.farzadafi.online_store.dto;
 
-import org.bson.types.Binary;
+import org.springframework.web.multipart.MultipartFile;
 
 public record ProductDto(String id,
                          String name,
-                         Long Price,
+                         Long price,
                          Integer quantity,
                          String brand,
                          String description,
                          Integer rate,
-                         Binary image,
-                         String subCategoryId) {
-}
+                         MultipartFile image,
+                         String subCategoryId) {}
