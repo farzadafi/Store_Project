dcb6404666e199b951416b45e5a78e09fe2e73b3
farzadafi <farzadafi40@gmail.com>
2023-06-02 16:57:39 +0330

    add modelsToDtos method

--- online-store/src/main/java/com/farzadafi/online_store/mapper/CategoryMapper.java
+++ online-store/src/main/java/com/farzadafi/online_store/mapper/CategoryMapper.java
@@ -5,11 +5,16 @@ import com.farzadafi.online_store.model.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
     Category dtoToModel(CategoryDto categoryDto);
+
     CategoryDto modelToDto(Category category);
+
+    List<CategoryDto> modelsToDtos(List<Category> categorieList);
 }
