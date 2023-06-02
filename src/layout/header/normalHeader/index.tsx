39369de0b778c0e4b2a1a24492cc5edefd4dfd85
farzadafi <farzadafi40@gmail.com>
2023-06-02 20:56:45 +0330

    fix icons

--- src/layout/header/normalHeader/index.tsx
+++ src/layout/header/normalHeader/index.tsx
@@ -1,14 +1,15 @@
 import {Button} from "@/component";
 import {SlBasket} from "react-icons/sl";
+import {FcMultipleSmartphones} from "react-icons/fc";
 
 const NormalHeader = () => {
   return (
     <div className={"flex justify-between bg-[#8130d1] p-2"}>
-      <div className={"flex items-center gap-3"}>
-        <img src="public/shop-64.png" alt="shop icon" className={"w-12"}/>
-        <p className={"text-1xl text-white"}>فروشگاه ناصر</p>
+      <div className={"flex items-center gap-2"}>
+        <FcMultipleSmartphones className={"text-2xl"}/>
+        <p className={"text-xs text-white"}>موبایل ناصر</p>
       </div>
-      <div className={"flex gap-4"}>
+      <div className={"flex gap-4 text-xs"}>
         <Button variant={"managerButton"}>مدیریت</Button>
         <Button variant={"managerButton"}>
           <div className={"flex gap-2 items-center"}>
