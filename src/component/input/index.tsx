1c1c276d3c30cd1438e4545b1f53252d4c0292bc
farzadafi <farzadafi40@gmail.com>
2023-06-03 10:40:55 +0330

    fix some classes

--- src/component/input/index.tsx
+++ src/component/input/index.tsx
@@ -11,13 +11,13 @@ const Input = ({placeHolder, icon, type, name}: Props) => {
   return (
     <div>
       <div className="relative rounded-md shadow-sm">
-        <div className="absolute inset-y-0 left-0 pl-44 flex items-center pointer-events-none">
+        <div className="absolute inset-y-0 left-0 pl-48 flex items-center pointer-events-none">
           {
             icon
           }
         </div>
         <input type={type} name={name}
-               className="focus:ring-indigo-500 pr-6 focus:border-indigo-500 block w-full pl-24 sm:text-sm border-gray-300 rounded-md"
+               className="focus:outline-pink-600 pr-8 outline-none block bg-transparent h-10 w-full sm:text-sm border border-gray-300 rounded-md"
                placeholder={placeHolder}/>
       </div>
     </div>
