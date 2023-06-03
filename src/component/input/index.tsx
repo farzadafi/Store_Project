1b4f872abd5011112bf8805df0e1434e772a2340
farzadafi <farzadafi40@gmail.com>
2023-06-03 10:13:57 +0330

    implement body of Input

--- src/component/input/index.tsx
+++ src/component/input/index.tsx
@@ -1,6 +1,25 @@
-const Input = () => {
+import {ReactNode} from "react";
+
+interface Props {
+  placeHolder: string,
+  icon: ReactNode,
+  type: string,
+  name: string
+}
+
+const Input = ({placeHolder, icon, type, name}: Props) => {
   return (
     <div>
+      <div className="relative rounded-md shadow-sm">
+        <div className="absolute inset-y-0 left-0 pl-44 flex items-center pointer-events-none">
+          {
+            icon
+          }
+        </div>
+        <input type={type} name={name}
+               className="focus:ring-indigo-500 pr-6 focus:border-indigo-500 block w-full pl-24 sm:text-sm border-gray-300 rounded-md"
+               placeholder={placeHolder}/>
+      </div>
     </div>
   );
 };
