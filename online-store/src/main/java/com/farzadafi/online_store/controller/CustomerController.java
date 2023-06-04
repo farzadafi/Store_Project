e36e1cda255c979be7452b45e2e5a7b72dd73242
farzadafi <farzadafi40@gmail.com>
2023-06-04 09:30:43 +0330

    add registerCustomer method

---
+++ online-store/src/main/java/com/farzadafi/online_store/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package com.farzadafi.online_store.controller;
+
+import com.farzadafi.online_store.dto.CustomerDto;
+import com.farzadafi.online_store.dto.ReturnMessage;
+import com.farzadafi.online_store.mapper.CustomerMapper;
+import com.farzadafi.online_store.model.Customer;
+import com.farzadafi.online_store.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public record CustomerController(CustomerService customerService) {
+
+    @PostMapping("/register")
+    public ResponseEntity<ReturnMessage> registerCustomer(@RequestBody CustomerDto customerDto) {
+        Customer customer = CustomerMapper.INSTANCE.dtoToModel(customerDto);
+        customerService.registerCustomer(customer);
+        return new ResponseEntity<>(new ReturnMessage("ok", 201), HttpStatus.CREATED);
+    }
+}
