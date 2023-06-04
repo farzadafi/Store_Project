67de9d3e168bf08d65734d08b00d9baf8267cb65
farzadafi <farzadafi40@gmail.com>
2023-06-04 10:51:52 +0330

    remove Builder annotatoin

--- online-store/src/main/java/com/farzadafi/online_store/model/Customer.java
+++ online-store/src/main/java/com/farzadafi/online_store/model/Customer.java
@@ -1,14 +1,15 @@
 package com.farzadafi.online_store.model;
 
 import lombok.*;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Builder
 @Document
+@TypeAlias("customer")
 public class Customer extends User {
     private Long balance;
 }
