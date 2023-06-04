ea75e5c50aa0f27cc3c49bbfe7dc19d84bb11fa8
farzadafi <farzadafi40@gmail.com>
2023-06-04 12:24:19 +0330

    fix @Document and add a name

--- online-store/src/main/java/com/farzadafi/online_store/model/Customer.java
+++ online-store/src/main/java/com/farzadafi/online_store/model/Customer.java
@@ -1,15 +1,13 @@
 package com.farzadafi.online_store.model;
 
 import lombok.*;
-import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Document
-@TypeAlias("customer")
+@Document("users")
 public class Customer extends User {
     private Long balance;
 }
