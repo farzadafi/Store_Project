973a6d3b1ac7f73cccd61f49f51693b6f235909e
farzadafi <farzadafi40@gmail.com>
2023-06-04 09:39:03 +0330

    remove AllArgsConstructor annotation

--- online-store/src/main/java/com/farzadafi/online_store/model/Admin.java
+++ online-store/src/main/java/com/farzadafi/online_store/model/Admin.java
@@ -3,7 +3,6 @@ package com.farzadafi.online_store.model;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
