1dca98e43810968af681c54d777f2d4e424e7e2f
farzadafi <farzadafi40@gmail.com>
2023-06-04 09:22:05 +0330

    create CustomerMapper interface

---
+++ online-store/src/main/java/com/farzadafi/online_store/mapper/CustomerMapper.java
@@ -0,0 +1,12 @@
+package com.farzadafi.online_store.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+
+}
