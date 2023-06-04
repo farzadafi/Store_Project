7fb850562184a73677d3510dc243eb2d3cfb3a71
farzadafi <farzadafi40@gmail.com>
2023-06-04 10:53:16 +0330

    fix registerCustomer method

--- online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
@@ -1,13 +1,18 @@
 package com.farzadafi.online_store.service;
 
 import com.farzadafi.online_store.model.Customer;
+import com.farzadafi.online_store.model.enumoration.Role;
 import com.farzadafi.online_store.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(Customer customer) {
+        customer.setId(UUID.randomUUID().toString());
+        customer.setRole(Role.CUSTOMER);
         customerRepository.save(customer).subscribe();
     }
 }
