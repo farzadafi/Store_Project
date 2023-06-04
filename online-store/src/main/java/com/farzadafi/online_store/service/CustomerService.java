3de085f50fcdec4cfc3f5e454ce4396c1136c5a5
farzadafi <farzadafi40@gmail.com>
2023-06-04 15:14:56 +0330

    fix registerCustomer method and encrypt password

--- online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
@@ -4,18 +4,21 @@ import com.farzadafi.online_store.exception.DuplicateException;
 import com.farzadafi.online_store.model.Customer;
 import com.farzadafi.online_store.model.enumoration.Role;
 import com.farzadafi.online_store.repository.CustomerRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository,
+                              BCryptPasswordEncoder passwordEncoder) {
 
     public void registerCustomer(Customer customer) {
         if (customerRepository.findByUsername(customer.getUsername()).block() != null)
             throw new DuplicateException(String.format("این نام کاربری قبلا استفاده شده است, %s", customer.getUsername()));
         customer.setId(UUID.randomUUID().toString());
         customer.setRole(Role.CUSTOMER);
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         customerRepository.save(customer).subscribe();
     }
 }
