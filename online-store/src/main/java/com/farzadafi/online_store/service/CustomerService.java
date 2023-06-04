a540e9c6d43f999cb03b15a967bd28141ea032f9
farzadafi <farzadafi40@gmail.com>
2023-06-04 12:39:49 +0330

    fix registerCustomer method

--- online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
+++ online-store/src/main/java/com/farzadafi/online_store/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.farzadafi.online_store.service;
 
+import com.farzadafi.online_store.exception.DuplicateException;
 import com.farzadafi.online_store.model.Customer;
 import com.farzadafi.online_store.model.enumoration.Role;
 import com.farzadafi.online_store.repository.CustomerRepository;
@@ -11,6 +12,8 @@ import java.util.UUID;
 public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(Customer customer) {
+        if (customerRepository.findByUsername(customer.getUsername()).block() != null)
+            throw new DuplicateException(String.format("این نام کاربری قبلا استفاده شده است, %s", customer.getUsername()));
         customer.setId(UUID.randomUUID().toString());
         customer.setRole(Role.CUSTOMER);
         customerRepository.save(customer).subscribe();
