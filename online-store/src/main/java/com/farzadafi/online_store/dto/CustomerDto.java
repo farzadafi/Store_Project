71b90561f607c750543a292d540af69de3b8493e
farzadafi <farzadafi40@gmail.com>
2023-06-04 09:21:00 +0330

    create CustomerDto and add some needed field

---
+++ online-store/src/main/java/com/farzadafi/online_store/dto/CustomerDto.java
@@ -0,0 +1,8 @@
+package com.farzadafi.online_store.dto;
+
+public record CustomerDto(String id,
+                          String firstname,
+                          String lastname,
+                          String username,
+                          String password) {
+}
