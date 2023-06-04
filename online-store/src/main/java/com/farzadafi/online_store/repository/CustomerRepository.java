1d1f9f498048840e4c3ab6254bcbb0bc58310614
farzadafi <farzadafi40@gmail.com>
2023-06-04 12:39:58 +0330

    add findByUsername method

--- online-store/src/main/java/com/farzadafi/online_store/repository/CustomerRepository.java
+++ online-store/src/main/java/com/farzadafi/online_store/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.farzadafi.online_store.repository;
 
 import com.farzadafi.online_store.model.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
+    Mono<Customer> findByUsername(String username);
 }
