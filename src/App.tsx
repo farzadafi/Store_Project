a0723ad54f8936b94440ac55fa3119f5ac0e52a5
farzadafi <farzadafi40@gmail.com>
2023-06-05 13:13:48 +0330

    add ToastContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Basket, HomePage, InventoryAndPrices} from "@/pages";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Container} from "@/containers";
 import {CookiesProvider} from "react-cookie";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
             <Route path={"/basket"} Component={Basket}></Route>
           </Routes>
         </Router>
+        <ToastContainer />
       </Container>
     </CookiesProvider>
   );
