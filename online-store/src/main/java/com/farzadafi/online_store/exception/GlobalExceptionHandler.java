23830618a97b8865af246cb86982be6a7cb75fee
farzadafi <farzadafi40@gmail.com>
2023-06-05 09:07:03 +0330

    add handleHttpClientErrorException

--- online-store/src/main/java/com/farzadafi/online_store/exception/GlobalExceptionHandler.java
+++ online-store/src/main/java/com/farzadafi/online_store/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +20,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ReturnMessage> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ReturnMessage(e.getMessage(), HttpStatus.NO_CONTENT.value()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ReturnMessage> handleHttpClientErrorException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ReturnMessage("unAuthorize", HttpStatus.UNAUTHORIZED.value()));
     }
 }
