e29f4d350d0f9e292a762570da41cde09009cd9e
farzadafi <farzadafi40@gmail.com>
2023-06-05 04:47:24 +0330

    add CookiesProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import {Basket, HomePage, InventoryAndPrices} from "@/pages";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Container} from "@/containers";
+import {CookiesProvider} from "react-cookie";
 
 function App() {
   return (
-    <Container>
-      <Router>
-        <Routes>
-          <Route path={"/"} Component={HomePage}></Route>
-          <Route path={"/inventory-and-prices"} Component={InventoryAndPrices}></Route>
-          <Route path={"/basket"} Component={Basket}></Route>
-        </Routes>
-      </Router>
-    </Container>
+    <CookiesProvider>
+      <Container>
+        <Router>
+          <Routes>
+            <Route path={"/"} Component={HomePage}></Route>
+            <Route path={"/inventory-and-prices"} Component={InventoryAndPrices}></Route>
+            <Route path={"/basket"} Component={Basket}></Route>
+          </Routes>
+        </Router>
+      </Container>
+    </CookiesProvider>
   );
 }
 
