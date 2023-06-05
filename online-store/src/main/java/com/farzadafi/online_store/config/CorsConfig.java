4782f3955c7c2a551ba463771c0009b0caf7b0f8
farzadafi <farzadafi40@gmail.com>
2023-06-05 09:28:41 +0330

    create CorsConfig class and add addCorsMappings method

---
+++ online-store/src/main/java/com/farzadafi/online_store/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.farzadafi.online_store.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowedHeaders("Authorization")
+                .allowedHeaders("authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
