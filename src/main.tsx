225b838d382ee3cf3b98b801a71b122b095ed96b
farzadafi <farzadafi40@gmail.com>
2023-06-05 15:19:46 +0330

    add RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {RouterProvider} from "react-router";
+import router from "@/routes";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    <ToastContainer/>
   </React.StrictMode>,
-)
+);
