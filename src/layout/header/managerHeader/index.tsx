7dd580136d44732eab29d970df5b34af8efb4101
farzadafi <farzadafi40@gmail.com>
2023-06-06 10:55:27 +0330

    implement body of ManagerHeader and add four button

--- src/layout/header/managerHeader/index.tsx
+++ src/layout/header/managerHeader/index.tsx
@@ -1,6 +1,21 @@
+import {Button} from "@/component";
+
 const ManagerHeader = () => {
   return (
-    <div>
+    <div className={"flex justify-between bg-[#8130d1] p-2 max-sm:gap-4"}>
+      <div className={"flex items-center"}>
+        <p className={"text-xs text-white max-sm:text-xl"}>پنل مدیریت فروشگاه</p>
+      </div>
+      <div className={"flex gap-4 text-xs max-[550px]:flex-col"}>
+        <div className={"flex gap-2"}>
+          <Button classes={""} variant={"managerButton"}>کالا ها</Button>
+          <Button variant={"managerButton"}>موجودی و قیمت ها</Button>
+        </div>
+        <div className={"flex gap-2"}>
+          <Button variant={"managerButton"}>سفارش ها</Button>
+          <Button variant={"managerButton"}>صفحه اول</Button>
+        </div>
+      </div>
     </div>
   );
 };
