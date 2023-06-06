caf0c8a3ac1683d6bfb852680d16da941dba998e
farzadafi <farzadafi40@gmail.com>
2023-06-06 15:41:38 +0330

    export Card

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './button'
 export { default as Input } from './input'
 export { default as FormikInput } from './formikInput'
+export { default as Card } from './card'
