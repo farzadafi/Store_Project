a272a59f4ab861443eadd7bc35a44f1b2d886594
farzadafi <farzadafi40@gmail.com>
2023-06-06 15:40:30 +0330

    add ManagerProduct component

--- src/pages/manager/mainPage/index.tsx
+++ src/pages/manager/mainPage/index.tsx
@@ -1,10 +1,12 @@
 import {ManagerHeader} from "@/layout";
 import {Container} from "@/containers";
+import ManagerProducts from "@/pages/manager/products/ManagerProducts";
 
 const MainManagerPage = () => {
   return (
     <Container>
       <ManagerHeader/>
+      <ManagerProducts/>
     </Container>
   );
 };
