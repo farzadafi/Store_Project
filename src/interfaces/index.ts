1ed5afc9a6a4988d4e463d7de4252c18848475c0
farzadafi <farzadafi40@gmail.com>
2023-06-06 15:40:55 +0330

    add SubCategory and Product interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -12,3 +12,21 @@ export interface ResultMessage {
   message: string,
   code: number
 }
+
+export interface SubCategory {
+  id: string;
+  name: string;
+  categoryId: string;
+  subCategories: Product[];
+}
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  description: string;
+  rate: number;
+  image: string;
+  subCategoryId: string;
+}
