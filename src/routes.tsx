ce1ad371a9aa1a6c3e13e667ca71b35e05ecd8ef
farzadafi <farzadafi40@gmail.com>
2023-06-07 12:18:49 +0330

    add new routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
-import {HomePage, InventoryAndPrices, LoginPage, MainManagerPage} from "@/pages";
+import {HomePage, InventoryAndPrices, LoginPage, MainManagerPage, Orders} from "@/pages";
 import ManagerProducts from "@/pages/manager/products/ManagerProducts";
 
 const router = createBrowserRouter([{path: "/", element: <HomePage/>, children: [{}]},
@@ -12,7 +12,8 @@ const router = createBrowserRouter([{path: "/", element: <HomePage/>, children: 
     element: <MainManagerPage/>,
     children: [
       {path: "products", element: <ManagerProducts/>},
-      {path: "inventory", element: <InventoryAndPrices/>}
+      {path: "inventory", element: <InventoryAndPrices/>},
+      {path: "orders", element: <Orders/>}
     ]
   }]);
 
