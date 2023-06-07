fb0664e0bad5feadd0f8fa085faaefcdf2486bc0
farzadafi <farzadafi40@gmail.com>
2023-06-07 07:45:50 +0330

    add a table for show fetch products

--- src/pages/manager/products/ManagerProducts.tsx
+++ src/pages/manager/products/ManagerProducts.tsx
@@ -1,4 +1,4 @@
-import {Button, Card} from "@/component";
+import {Button} from "@/component";
 import ApiClient from "@/services/api-client";
 import {SubCategory} from "@/interfaces";
 import {useEffect, useState} from "react";
@@ -6,6 +6,8 @@ import {useEffect, useState} from "react";
 const ManagerProducts = () => {
   const [fetchData, setFetchData] = useState<SubCategory[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const tableHeaderArray = ["تصویر", "نام کالا", "دسته بندی", "عملیات"];
 
   useEffect(() => {
     const fetchSubCategories = async () => {
@@ -52,23 +54,44 @@ const ManagerProducts = () => {
   }
 
   return (
-    <div>
-      <div className={"flex justify-between items-center p-2"}>
+    <div className={"flex flex-col justify-center items-center"}>
+      <div className={"flex justify-between items-center p-2 w-full"}>
         <p>مدریریت کالا ها</p>
         <Button classes={"max-sm:h-8"} variant={"managerButton"}>افزودن کالا</Button>
       </div>
-      <div>
-        {fetchData.map(subCategory => (
-          <div key={subCategory.id}>
-            <div>
-              {subCategory.subCategories.map(product => (
-                <Card key={product.id} image={product.image} name={product.name} price={product.price}
-                      subCategory={subCategory.name}/>
+
+      <div className={"max-w-4xl mt-10"}>
+        <div className="relative overflow-y-auto h-80 overflow-hidden rounded-xl border">
+          <table className="w-full text-sm text-left text-white dark:text-gray-400 table-fixed">
+            <thead className="text-xs text-white uppercase dark:bg-gray-700 dark:text-gray-400 border-b">
+            <tr>
+              {tableHeaderArray.map((str, index) => (
+                <th scope="col" className="py-3 text-center" key={index}>
+                  {str}
+                </th>
               ))}
-            </div>
-          </div>
-        ))}
+            </tr>
+            </thead>
+            <tbody className={""}>
+            {fetchData.map((subCategory) => (
+              subCategory.subCategories.map((product, index) => (
+                  <tr className={"border-b dark:bg-gray-800 dark:border-gray-700"} key={index}>
+                    <td className=" whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-6">
+                      <img className="w-12 h-12 flex-shrink-0 mx-auto" src={"data:image/png;base64," + product.image}
+                           alt="imageUrl"/>
+                    </td>
+                    <td className="">{product.name}</td>
+                    <td className="">{subCategory.name}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm">{"farzad"}</td>
+                  </tr>
+                )
+              )))}
+            </tbody>
+          </table>
+        </div>
       </div>
+
+
     </div>
   );
 };
