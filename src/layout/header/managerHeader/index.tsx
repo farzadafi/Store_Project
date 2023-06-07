34fc9448535aaac5b18b5696b71e6365f6ea241d
farzadafi <farzadafi40@gmail.com>
2023-06-07 12:18:22 +0330

    add three function for handle route and fix classes for all button

--- src/layout/header/managerHeader/index.tsx
+++ src/layout/header/managerHeader/index.tsx
@@ -11,6 +11,18 @@ const ManagerHeader = () => {
     navigate('inventory')
   }
 
+  const productsHandle = () => {
+    navigate('products')
+  }
+
+  const ordersHandle = () => {
+    navigate('orders')
+  }
+
+  const homeHandle = () => {
+    navigate('/')
+  }
+
   return (
     <div className={"flex justify-between bg-[#8130d1] p-2 max-sm:gap-4"}>
       <div className={"flex items-center"}>
@@ -18,12 +30,12 @@ const ManagerHeader = () => {
       </div>
       <div className={"flex gap-4 text-xs max-[550px]:flex-col"}>
         <div className={"flex gap-2"}>
-          <Button classes={`max-sm:whitespace-nowrap ${currentRoute === "main-manager-page" ? "" : "bg-green-500"}`} variant={"managerButton"}>کالا ها</Button>
-          <Button onClick={inventoryProductsHandle} classes={"max-sm:whitespace-nowrap"} variant={"managerButton"}>موجودی و قیمت ها</Button>
+          <Button onClick={productsHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === "/main-manager-page/products" ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>کالا ها</Button>
+          <Button onClick={inventoryProductsHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === '/main-manager-page/inventory' ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>موجودی و قیمت ها</Button>
         </div>
         <div className={"flex gap-2"}>
-          <Button variant={"managerButton"}>سفارش ها</Button>
-          <Button variant={"managerButton"}>صفحه اول</Button>
+          <Button onClick={ordersHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === '/main-manager-page/orders' ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>سفارش ها</Button>
+          <Button onClick={homeHandle} variant={"managerButton"}>صفحه اول</Button>
         </div>
       </div>
     </div>
