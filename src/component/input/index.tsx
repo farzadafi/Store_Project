61602533c3f31e1429f80edb6c1ba6aeb272e067
farzadafi <farzadafi40@gmail.com>
2023-06-08 15:24:58 +0330

    add new function and string to interface and Input component

--- src/component/input/index.tsx
+++ src/component/input/index.tsx
@@ -5,9 +5,11 @@ interface Props {
   icon: ReactNode,
   type: string,
   name: string,
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;}
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  classes?: string
+}
 
-const Input = ({placeHolder, icon, type, name, onChange}: Props) => {
+const Input = ({placeHolder, icon, type, name, onChange, classes}: Props) => {
   return (
     <div>
       <div className="relative rounded-md shadow-sm">
@@ -17,7 +19,7 @@ const Input = ({placeHolder, icon, type, name, onChange}: Props) => {
           }
         </div>
         <input type={type} name={name} onChange={onChange}
-               className="focus:outline-pink-600 pr-8 outline-none block bg-transparent h-10 w-full sm:text-sm border border-gray-300 rounded-md"
+               className={`focus:outline-pink-600 pr-8 outline-none block bg-transparent h-10 w-full sm:text-sm border border-gray-300 rounded-md ${classes}`}
                placeholder={placeHolder}/>
       </div>
     </div>
