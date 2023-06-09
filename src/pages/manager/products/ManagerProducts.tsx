d5544ab2612d7672037ec2254f9c7393d1e0f242
farzadafi <farzadafi40@gmail.com>
2023-06-09 15:35:33 +0330

    add a simple pagination

--- src/pages/manager/products/ManagerProducts.tsx
+++ src/pages/manager/products/ManagerProducts.tsx
@@ -4,21 +4,18 @@ import {FetchProduct, SubCategory} from "@/interfaces";
 import React, {useEffect, useRef, useState} from "react";
 import {BsPencilSquare} from "react-icons/bs";
 import {RiDeleteBin6Line} from "react-icons/ri";
-import {IoIosArrowDown} from "react-icons/io";
+import {IoIosArrowDown, IoMdArrowRoundBack, IoMdArrowRoundForward} from "react-icons/io";
 import {BiSearchAlt} from "react-icons/bi";
+import {toast} from "react-toastify";
 
 const ManagerProducts = () => {
-  const searchIcon = <BiSearchAlt className={"text-gray-500 w-4 h-4"}/>;
-  const [fetchData, setFetchData] = useState<FetchProduct[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [sourceOfTruth, setSourceOfTruth] = useState<FetchProduct[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
-  // const [currentPage, setCurrentPage] = useState(1);
-  // let totalProduct = 0;
-  // let totalPages = 0;
-  // let paginatedProducts = []
-
-  // console.log(totalPages);
+    const searchIcon = <BiSearchAlt className={"text-gray-500 w-4 h-4"}/>;
+    const [fetchData, setFetchData] = useState<FetchProduct[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [sourceOfTruth, setSourceOfTruth] = useState<FetchProduct[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
+    const [currentPage, setCurrentPage] = useState(1);
+    let totalProduct = 0;
 
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
@@ -88,6 +85,27 @@ const ManagerProducts = () => {
     }
   };
 
+  const showErrorToastMessage = () => {
+    toast.warning("تو صفحه لاگین بهت گفتم یه چیزی میزنی :|", {
+      position: toast.POSITION.TOP_RIGHT,
+      className: "toast-message"
+    });
+  };
+
+  const forwardButtonHandle = () => {
+    if (currentPage * 10 - 10 + 3 > totalProduct) {
+      showErrorToastMessage();
+    } else
+      setCurrentPage(currentPage + 1);
+  };
+
+  const backButtonHandle = () => {
+    if (currentPage * 10 - 10 === 0)
+      showErrorToastMessage();
+    else
+      setCurrentPage(currentPage - 1);
+  };
+
 
   useEffect(() => {
     const fetchSubCategories = async () => {
@@ -104,10 +122,6 @@ const ManagerProducts = () => {
         });
         setFetchData(formattedResult);
         setSourceOfTruth(formattedResult);
-        // totalProduct = formattedResult.reduce((total, subCategory) => {
-        //   return total + subCategory.subCategories.reduce((subtotal:number, product:Product) => subtotal + (product.quantity || 0), 0);
-        // }, 0) / 10;
-        // totalPages = Math.ceil(totalProduct / 10);
         setLoading(false);
       } catch (error) {
         console.error(error);
@@ -123,97 +137,123 @@ const ManagerProducts = () => {
         <div>مدیر باید صبور باشه...</div>
       </div>
     );
-  }
+  } else {
+    totalProduct = fetchData.length;
+    }
 
-  return (
-    <div className={"flex flex-col justify-center items-center"}>
-      <div className={"flex justify-between items-center p-2 w-full"}>
-        <p>مدریریت کالا ها</p>
-        <Button classes={"max-sm:h-8"} variant={"managerButton"}>افزودن کالا</Button>
-      </div>
-      <div className={"max-w-4xl mt-10"}>
-        <div className="relative overflow-y-auto  h-[40rem] max-sm:h-[30rem] overflow-hidden rounded-xl border">
-          <table className="w-full max-sm:w-screen text-sm text-white dark:text-gray-400 table-fixed">
-            <thead className="text-xs text-white uppercase dark:bg-gray-700 dark:text-gray-400 border-b">
-            <tr>
-              <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>تصویر</th>
-              <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>
-                <Input placeHolder={"نام محصول"} icon={searchIcon} type={"text"} name={"search"}
-                       onChange={handleInputChange} classes={"bg-white text-black h-8 max-w"}/>
-              </th>
-              <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>
-                <div className={"relative"} ref={handleDropdownRef}>
-                  <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
-                          className="text-gray-500 bg-white hover:bg-gray-100 rounded-lg text-sm px-3 py-1.5 "
-                          type="button" onClick={toggleDropdown}>
-                    <p className={"inline-block"}>دسته بندی ها</p>
-                    <IoIosArrowDown className={"inline-block mr-2"}/>
-                  </button>
-                  {isOpen && (
-                    <div id="dropdownRadio"
-                         className="absolute left-[3.9rem] w-24 z-10 shadow dark:bg-gray-700 dark:divide-gray-600"
-                         data-popper-placement="top">
-                      <ul className="text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
-                        {[{
-                          id: "",
-                          name: "",
-                          image: "",
-                          subCategoryName: "همه"
-                        }, ...sourceOfTruth]
-                          .map((str, _index) => str.subCategoryName)
-                          .filter((value, index, self) => self.indexOf(value) === index)
-                          .map((uniqueValue, index) => (
-                            <li key={index}>
-                              <div
-                                className="flex items-center p-1 border-b border-gray-500 rounded-md gap-1 hover:bg-gray-100 bg-white dark:hover:bg-gray-600">
-                                <input onClick={filterData} id={`filter-radio-${uniqueValue}`} type="radio" value=""
-                                       name="filter-radio"
-                                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                                <label htmlFor={`filter-radio-${uniqueValue}`}
-                                       className="whitespace-nowrap w-full ml-2 text-xs text-gray-900 rounded dark:text-gray-300">{uniqueValue}</label>
-                              </div>
-                            </li>
-                          ))}
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              </th>
-              <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>عملیات</th>
-            </tr>
-            </thead>
-            <tbody className={""}>
-            {
-              // fetchData.slice(currentPage * 10 - 10, currentPage * 10).map((subCategory) => (
-              fetchData.map((product, index) => (
-                  <tr className={"border-b dark:bg-gray-800 dark:border-gray-700 text-center max-sm:text-right"}
-                      key={index}>
-                    <td
-                      className="whitespace-nowrap pl-4 pr-3 text-sm max-sm:pl-12 font-medium text-white sm:pl-2 max-sm:text-right">
-                      <img className="w-12 h-12 max-sm:w-8 max-sm:h-8 flex-shrink-0 mx-auto"
-                           src={"data:image/png;base64," + product.image} alt="imageUrl"/>
-                    </td>
-                    <td className="whitespace-nowrap max-sm:pl-24">{product.name}</td>
-                    <td className="max-sm:pr-2">{product.subCategoryName}</td>
-                    <td className="whitespace-nowrap py-4 text-sm flex">
-                      <Button classes={"max-sm:p-0"} variant={"edit"}>ویرایش
-                        <BsPencilSquare/>
-                      </Button>
-                      <Button classes={"max-sm:p-0"} variant={"remove"}>حذف
-                        <RiDeleteBin6Line/>
-                      </Button>
-                    </td>
-                  </tr>
-                )
-              )
-            }
-            </tbody>
-          </table>
+    return (
+      <div className={"flex flex-col justify-center items-center"}>
+        <div className={"flex justify-between items-center p-2 w-full"}>
+          <p>مدریریت کالا ها</p>
+          <Button classes={"max-sm:h-8"} variant={"managerButton"}>افزودن کالا</Button>
         </div>
+        <div className={"max-w-4xl mt-10"}>
+          <div className="relative overflow-y-auto  h-[30rem] max-sm:h-[30rem] overflow-hidden rounded-xl border">
+            <table className="w-full max-sm:w-screen text-sm text-white dark:text-gray-400 table-fixed">
+              <thead className="text-xs text-white uppercase dark:bg-gray-700 dark:text-gray-400 border-b">
+              <tr>
+                <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>تصویر</th>
+                <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>
+                  <Input placeHolder={"نام محصول"} icon={searchIcon} type={"text"} name={"search"}
+                         onChange={handleInputChange} classes={"bg-white text-black h-8 max-w"}/>
+                </th>
+                <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>
+                  <div className={"relative"} ref={handleDropdownRef}>
+                    <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
+                            className="text-gray-500 bg-white hover:bg-gray-100 rounded-lg text-sm px-3 py-1.5 "
+                            type="button" onClick={toggleDropdown}>
+                      <p className={"inline-block"}>دسته بندی ها</p>
+                      <IoIosArrowDown className={"inline-block mr-2"}/>
+                    </button>
+                    {isOpen && (
+                      <div id="dropdownRadio"
+                           className="absolute left-[3.9rem] w-24 z-10 shadow dark:bg-gray-700 dark:divide-gray-600"
+                           data-popper-placement="top">
+                        <ul className="text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioButton">
+                          {[{
+                            id: "",
+                            name: "",
+                            image: "",
+                            subCategoryName: "همه"
+                          }, ...sourceOfTruth]
+                            .map((str, _index) => str.subCategoryName)
+                            .filter((value, index, self) => self.indexOf(value) === index)
+                            .map((uniqueValue, index) => (
+                              <li key={index}>
+                                <div
+                                  className="flex items-center p-1 border-b border-gray-500 rounded-md gap-1 hover:bg-gray-100 bg-white dark:hover:bg-gray-600">
+                                  <input onClick={filterData} id={`filter-radio-${uniqueValue}`} type="radio" value=""
+                                         name="filter-radio"
+                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                  <label htmlFor={`filter-radio-${uniqueValue}`}
+                                         className="whitespace-nowrap w-full ml-2 text-xs text-gray-900 rounded dark:text-gray-300">{uniqueValue}</label>
+                                </div>
+                              </li>
+                            ))}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                </th>
+                <th className={"py-3 sm:text-center max-sm:text-right max-sm:p-3"}>عملیات</th>
+              </tr>
+              </thead>
+              <tbody className={""}>
+              {
+                fetchData.slice(currentPage * 10 - 10, currentPage * 10).map((product, index) => (
+                    // fetchData.map((product, index) => (
+                    <tr className={"border-b dark:bg-gray-800 dark:border-gray-700 text-center max-sm:text-right"}
+                        key={index}>
+                      <td
+                        className="whitespace-nowrap pl-4 pr-3 text-sm max-sm:pl-12 font-medium text-white sm:pl-2 max-sm:text-right">
+                        <img className="w-12 h-12 max-sm:w-8 max-sm:h-8 flex-shrink-0 mx-auto"
+                             src={"data:image/png;base64," + product.image} alt="imageUrl"/>
+                      </td>
+                      <td className="whitespace-nowrap max-sm:pl-24">{product.name}</td>
+                      <td className="max-sm:pr-2">{product.subCategoryName}</td>
+                      <td className="whitespace-nowrap py-4 text-sm flex">
+                        <Button classes={"max-sm:p-0"} variant={"edit"}>ویرایش
+                          <BsPencilSquare/>
+                        </Button>
+                        <Button classes={"max-sm:p-0"} variant={"remove"}>حذف
+                          <RiDeleteBin6Line/>
+                        </Button>
+                      </td>
+                    </tr>
+                  )
+                )
+              }
+              </tbody>
+            </table>
+          </div>
 
+          <nav className="flex items-center justify-between pt-4" aria-label="Table navigation">
+          <span className="text-sm font-normal text-white dark:text-gray-400">موارد <span
+            className="font-semibold text-black dark:text-white">{currentPage * 10 - 10 + 1}{" - "}{currentPage * 10}</span> از <span
+            className="font-semibold text-black dark:text-white">{totalProduct}</span> <span>محصول</span>   </span>
+            <ul className="inline-flex items-center -space-x-px">
+              <li>
+                <a onClick={forwardButtonHandle}
+                   className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="sr-only">Next</span>
+                  <IoMdArrowRoundForward/>
+                </a>
+              </li>
+
+              <li>
+                <a onClick={backButtonHandle}
+                   className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="sr-only">Previous</span>
+                  <IoMdArrowRoundBack/>
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+;
 
 export default ManagerProducts;
