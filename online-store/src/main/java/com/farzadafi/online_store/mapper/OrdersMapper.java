dfa0bb68e771fb1ca6cb148d8c0f33932104d088
farzadafi <farzadafi40@gmail.com>
2023-06-10 05:49:10 +0330

    add modelsToDtos method

--- online-store/src/main/java/com/farzadafi/online_store/mapper/OrdersMapper.java
+++ online-store/src/main/java/com/farzadafi/online_store/mapper/OrdersMapper.java
@@ -1,9 +1,12 @@
 package com.farzadafi.online_store.mapper;
 
 import com.farzadafi.online_store.dto.OrdersDto;
+import com.farzadafi.online_store.dto.OrdersDtoResponse;
 import com.farzadafi.online_store.model.Orders;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface OrdersMapper {
@@ -11,4 +14,6 @@ public interface OrdersMapper {
     OrdersMapper INSTANCE = Mappers.getMapper(OrdersMapper.class);
 
     Orders dtoToModel(OrdersDto ordersDto);
+
+    List<OrdersDtoResponse> modelsToDtos(List<Orders> ordersList);
 }
