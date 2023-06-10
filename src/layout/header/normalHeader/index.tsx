46dd69ed74adf47cd26fea22fe9dbd57a1687d2f
farzadafi <farzadafi40@gmail.com>
2023-06-10 14:27:59 +0330

    add handleManageButton

--- src/layout/header/normalHeader/index.tsx
+++ src/layout/header/normalHeader/index.tsx
@@ -1,9 +1,35 @@
 import {Button} from "@/component";
 import {SlBasket} from "react-icons/sl";
 import {FcMultipleSmartphones} from "react-icons/fc";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router";
+import {useCookies} from "react-cookie";
+import ApiClient from "@/services/api-client";
+import {ResultMessage} from "@/interfaces";
+
 
 const NormalHeader = () => {
+  const navigate = useNavigate();
+  const [cookies] = useCookies(["token"]);
+
+
+  const handleManageButton = () => {
+    async function verifyToken() {
+      if (cookies.token) {
+        try {
+          const instance = new ApiClient("/api/login/verify-token");
+          const result = await instance.verifyToken(cookies.token) as Promise<ResultMessage>;
+          console.log(result);
+          navigate("/main-manager-page");
+        } catch (error) {
+          navigate("/login");
+        }
+      }
+    }
+
+    verifyToken();
+  };
+
+
   return (
     <div className={"flex justify-between bg-[#8130d1] p-2"}>
       <div className={"flex items-center gap-2"}>
@@ -11,9 +37,7 @@ const NormalHeader = () => {
         <p className={"text-xs text-white"}>موبایل ناصر</p>
       </div>
       <div className={"flex gap-4 text-xs"}>
-        <Link to="/login">
-          <Button variant={"managerButton"}>مدیریت</Button>
-        </Link>
+        <Button onClick={handleManageButton} variant={"managerButton"}>مدیریت</Button>
         <Button variant={"managerButton"}>
           <div className={"flex gap-2 items-center"}>
             <SlBasket/>
