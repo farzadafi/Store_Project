436d2f8832efddcd66e8b81a57718afe7cbdbde2
farzadafi <farzadafi40@gmail.com>
2023-06-11 13:36:07 +0330

    export * from modal dir

--- src/component/index.ts
+++ src/component/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './button'
 export { default as Input } from './input'
 export { default as FormikInput } from './formikInput'
 export { default as Card } from './card'
+export * from "./modal";
