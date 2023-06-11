240c870456836b91d1e0861e8e86e8ae1c8e8ff2
farzadafi <farzadafi40@gmail.com>
2023-06-11 15:42:20 +0330

    fix class of text area

--- src/component/modal/addProductModal/index.tsx
+++ src/component/modal/addProductModal/index.tsx
@@ -2,18 +2,16 @@ import {Button, FormikInput} from "@/component";
 import {IoIosCloseCircleOutline} from "react-icons/io";
 import {ErrorMessage, Form, Formik} from "formik";
 import {
-  Orders,
   ProductFormValue,
   ProductSaveError,
   ResultMessage, SubCategoryName,
 } from "@/interfaces";
 import ApiClient from "@/services/api-client";
-import {MdDriveFileRenameOutline, MdOutlineDescription} from "react-icons/md";
+import {MdDriveFileRenameOutline} from "react-icons/md";
 import {useEffect, useState} from "react";
 
 const closeIcon = <IoIosCloseCircleOutline className={"w-5 h-5 text-red-500"}/>;
 const productName = <MdDriveFileRenameOutline className={"text-gray-500"}/>;
-const descriptionIcon = <MdOutlineDescription className={"text-gray-500"}/>;
 
 const initialValues: ProductFormValue = {
   productImage: null,
@@ -117,7 +115,7 @@ const AddProductModal = ({handleClose}) => {
                     </div>
                     <div>
                       <textarea
-                        className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="توضیحات"></textarea>
                       <ErrorMessage name="description" component="div" className={"text-red-500 text-xs"}/>
                     </div>
