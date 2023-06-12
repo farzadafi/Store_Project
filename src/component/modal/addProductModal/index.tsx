74fee0af38389b822216b84f94a824c30e4fc668
farzadafi <farzadafi40@gmail.com>
2023-06-12 06:28:25 +0330

    add a textArea for product description

--- src/component/modal/addProductModal/index.tsx
+++ src/component/modal/addProductModal/index.tsx
@@ -4,11 +4,11 @@ import {ErrorMessage, Field, Form, Formik} from "formik";
 import {
   ProductFormValue,
   ProductSaveError,
-  ResultMessage, SubCategoryName,
+  SubCategoryName,
 } from "@/interfaces";
 import ApiClient from "@/services/api-client";
 import {MdDriveFileRenameOutline} from "react-icons/md";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const closeIcon = <IoIosCloseCircleOutline className={"w-5 h-5 text-red-500"}/>;
 const productName = <MdDriveFileRenameOutline className={"text-gray-500"}/>;
@@ -19,6 +19,16 @@ const initialValues: ProductFormValue = {
   categoryId: "",
   description: ""
 };
+
+
+interface TextareaFieldProps {
+  field: {
+    name: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    onBlur: () => void;
+  };
+}
 
 
 const AddProductModal = ({handleClose}) => {
@@ -60,14 +70,14 @@ const AddProductModal = ({handleClose}) => {
               else if (/^.{0,3}$/.test(values.productName))
                 errors.productName = "تو زندگیت سعی کن آدم باشی";
 
-              if(values.categoryId === '') {
-                errors.categoryId = "سلکت بار نرفت تو چشت؟"
+              if (values.categoryId === "") {
+                errors.categoryId = "سلکت بار نرفت تو چشت؟";
               }
 
-              // if(!values.description)
-              //   errors.description = "بنویس چند تا جمله دیگه"
-              // else if(/^.{0, 20}$/.test(values.description))
-              //   errors.description = "۲۰ تا کاراکتر بنویس انصافا"
+              if(!values.description)
+                errors.description = "بنویس چند تا جمله دیگه"
+              else if(/^.{0,20}$/.test(values.description))
+                errors.description = "۲۰ تا کاراکتر بنویس انصافا"
 
               // if(!values.productImage)
               //   errors.productImage = "هوووووووووووووووووی"
@@ -89,33 +99,34 @@ const AddProductModal = ({handleClose}) => {
               {() => (
                 <Form>
                   <div className={"flex flex-col gap-6"}>
-                    {/*<div>*/}
-                    {/*  <div className="flex items-center justify-center w-full">*/}
-                    {/*    <label htmlFor="dropzone-file"*/}
-                    {/*           className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">*/}
-                    {/*      <div className="flex flex-col items-center justify-center pt-5 pb-6">*/}
-                    {/*        <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none"*/}
-                    {/*             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">*/}
-                    {/*          <path*/}
-                    {/*            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>*/}
-                    {/*        </svg>*/}
-                    {/*        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span*/}
-                    {/*          className="font-semibold">برای آپلود عکس کلیک کنید</span> یا*/}
-                    {/*          تصویر را کشیده و در اینجا رها کنید</p>*/}
-                    {/*        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX 300KB)</p>*/}
-                    {/*      </div>*/}
-                    {/*      <input id="dropzone-file" type="file" className="hidden"/>*/}
-                    {/*    </label>*/}
-                    {/*  </div>*/}
-                    {/*  <ErrorMessage name="productImage" component="div" className={"text-red-500 text-xs"}/>*/}
-                    {/*</div>*/}
+                    <div>
+                      <div className="flex items-center justify-center w-full">
+                        <label htmlFor="dropzone-file"
+                               className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none"
+                                 stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                              <path
+                                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+                            </svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                              className="font-semibold">برای آپلود عکس کلیک کنید</span> یا
+                              تصویر را کشیده و در اینجا رها کنید</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX 300KB)</p>
+                          </div>
+                          <input id="dropzone-file" type="file" className="hidden"/>
+                        </label>
+                      </div>
+                    </div>
                     <div>
                       <FormikInput variant={"addProduct"} icon={productName} type={"text"} name={"productName"}
                                    placeHolder={"نام کالا"}/>
                       <ErrorMessage name="productName" component="div" className={"text-red-500 text-xs mt-2"}/>
                     </div>
                     <div>
-                      <Field className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"} as={"select"} name={"categoryId"}>
+                      <Field
+                        className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
+                        as={"select"} name={"categoryId"}>
                         {[
                           <option key="0" value="">
                             دسته بندی </option>,
@@ -130,12 +141,19 @@ const AddProductModal = ({handleClose}) => {
                       </Field>
                       <ErrorMessage name="categoryId" component="div" className={"text-red-500 text-xs mt-2"}/>
                     </div>
-                    {/*<div>*/}
-                    {/*  <textarea*/}
-                    {/*    className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"*/}
-                    {/*    placeholder="توضیحات"></textarea>*/}
-                    {/*  <ErrorMessage name="description" component="div" className={"text-red-500 text-xs"}/>*/}
-                    {/*</div>*/}
+                    <div>
+                      <Field name="description">
+                        {({ field}:TextareaFieldProps) => (
+                          <textarea
+                            {...field}
+                            className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="توضیحات"
+                          />
+                        )}
+                      </Field>
+                      <ErrorMessage name="description" component="div" className="text-red-500 text-xs" />
+                    </div>
+
                     <div className={"flex gap-2"}>
                       <div className={"w-[20rem]"}>
                         <Button onClick={handleClose} type={"button"} classes={"bg-red-500 w-full flex justify-center"}
