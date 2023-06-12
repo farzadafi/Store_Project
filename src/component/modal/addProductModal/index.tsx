add1cb08562484e144ce0e201d1699efb92fedc0
farzadafi <farzadafi40@gmail.com>
2023-06-12 08:12:05 +0330

    add description and two button

--- src/component/modal/addProductModal/index.tsx
+++ src/component/modal/addProductModal/index.tsx
@@ -13,12 +13,12 @@ import React, {useEffect, useState} from "react";
 const closeIcon = <IoIosCloseCircleOutline className={"w-5 h-5 text-red-500"}/>;
 const productName = <MdDriveFileRenameOutline className={"text-gray-500"}/>;
 
-// const initialValues: ProductFormValue = {
-//   productImage: null,
-//   productName: "",
-//   categoryId: "",
-//   description: ""
-// };
+const initialValues: ProductFormValue = {
+  productImage: null,
+  productName: "",
+  categoryId: "",
+  description: ""
+};
 
 
 interface TextareaFieldProps {
@@ -46,10 +46,6 @@ const AddProductModal = ({handleClose}) => {
     fetchSubCategories();
   }, []);
 
-  interface Props {
-    form: FormikProps<ProductFormValue>;
-  }
-
   const formik = useFormik({
     initialValues: {
       productImage: "",
@@ -73,184 +69,29 @@ const AddProductModal = ({handleClose}) => {
                 }
               </Button>
             </div>
+            <Formik initialValues={initialValues}
+                    validate={values => {
+                      const errors: ProductSaveError = {};
+                      if (!formik.values.productImage)
+                        errors.productImage = "هوووووووووووووووی";
 
+                      if (!values.productName)
+                        errors.productName = "چی میزنی؟";
+                      else if (/^.{0,3}$/.test(values.productName))
+                        errors.productName = "تو زندگیت سعی کن آدم باشی";
 
-            {/*<Formik initialValues={initialValues} validate={values => {*/}
-            {/*  const errors: ProductSaveError = {};*/}
-            {/*  if (!values.productName)*/}
-            {/*    errors.productName = "چی میزنی؟";*/}
-            {/*  else if (/^.{0,3}$/.test(values.productName))*/}
-            {/*    errors.productName = "تو زندگیت سعی کن آدم باشی";*/}
+                      if (values.categoryId === "") {
+                        errors.categoryId = "سلکت بار نرفت تو چشت؟";
+                      }
 
-            {/*  if (values.categoryId === "") {*/}
-            {/*    errors.categoryId = "سلکت بار نرفت تو چشت؟";*/}
-            {/*  }*/}
+                      if (!values.description)
+                        errors.description = "بنویس چند تا جمله دیگه";
+                      else if (/^.{0,20}$/.test(values.description))
+                        errors.description = "۲۰ تا کاراکتر بنویس انصافا";
 
-            {/*  if(!values.description)*/}
-            {/*    errors.description = "بنویس چند تا جمله دیگه"*/}
-            {/*  else if(/^.{0,20}$/.test(values.description))*/}
-            {/*    errors.description = "۲۰ تا کاراکتر بنویس انصافا"*/}
+                      return errors;
+                    }}
 
-            {/*// if(!values.productImage)*/}
-            {/*//   errors.productImage = "هوووووووووووووووووی"*/}
-            {/*  console.log(values.productImage);*/}
-            {/*  return errors;*/}
-            {/*}}*/}
-
-            {/*        onSubmit={(values, {setSubmitting}) => {*/}
-            {/*          setTimeout(() => {*/}
-            {/*            console.log(values);*/}
-            {/*            // const instance = new ApiClient("/api/login/getToken");*/}
-            {/*            // const resultCall = instance.loginPost(values) as Promise<ResultMessage>;*/}
-            {/*            // resultCall.then((result: ResultMessage) => {*/}
-            {/*            // }).catch((error: ResultMessage) => {*/}
-            {/*            // });*/}
-            {/*            setSubmitting(false);*/}
-            {/*          }, 400);*/}
-            {/*        }}>*/}
-
-            {/*{() => (*/}
-            {/*  // <Form>*/}
-            {/*<div className={"flex flex-col gap-6"}>*/}
-            {/*<div>*/}
-            {/*  <div className="flex items-center justify-center w-full">*/}
-            {/*    <label htmlFor="dropzone-file"*/}
-            {/*           className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">*/}
-            {/*      <div className="flex flex-col items-center justify-center pt-5 pb-6">*/}
-            {/*        <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none"*/}
-            {/*             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*          <path*/}
-            {/*            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>*/}
-            {/*        </svg>*/}
-            {/*        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span*/}
-            {/*          className="font-semibold">برای آپلود عکس کلیک کنید</span> یا*/}
-            {/*          تصویر را کشیده و در اینجا رها کنید</p>*/}
-            {/*        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX 300KB)</p>*/}
-            {/*      </div>*/}
-            {/*      <input id="dropzone-file" type="file" className="hidden"/>*/}
-            {/*    </label>*/}
-            {/*  </div>*/}
-            {/*  <ErrorMessage name="productImage" component="div" className={"text-red-500 text-xs mt-2"}/>*/}
-            {/*</div>*/}
-
-            {/*<div>*/}
-            {/*  <Field name="productImage">*/}
-            {/*    {({ field }) => (*/}
-            {/*      <div>*/}
-            {/*        <div className="flex items-center justify-center w-full">*/}
-            {/*          <label*/}
-            {/*            htmlFor="dropzone-file"*/}
-            {/*            className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"*/}
-            {/*          >*/}
-            {/*            <div className="flex flex-col items-center justify-center pt-5 pb-6">*/}
-            {/*              <svg*/}
-            {/*                aria-hidden="true"*/}
-            {/*                className="w-10 h-10 mb-3 text-gray-400"*/}
-            {/*                fill="none"*/}
-            {/*                stroke="currentColor"*/}
-            {/*                viewBox="0 0 24 24"*/}
-            {/*                xmlns="http://www.w3.org/2000/svg"*/}
-            {/*              >*/}
-            {/*                <path d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>*/}
-            {/*              </svg>*/}
-            {/*              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">*/}
-            {/*                <span className="font-semibold">برای آپلود عکس کلیک کنید</span> یا تصویر را کشیده و در اینجا*/}
-            {/*                رها کنید*/}
-            {/*              </p>*/}
-            {/*              <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX 300KB)</p>*/}
-            {/*            </div>*/}
-            {/*            <input*/}
-            {/*              id="dropzone-file"*/}
-            {/*              type="file"*/}
-            {/*              className="hidden"*/}
-            {/*              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {*/}
-            {/*                formik.setFieldValue('productImage', event.currentTarget.files?.[0]);*/}
-            {/*              }}*/}
-            {/*            />*/}
-            {/*          </label>*/}
-            {/*        </div>*/}
-            {/*        <ErrorMessage name="productImage" component="div" className={"text-red-500 text-xs mt-2"} />*/}
-            {/*      </div>*/}
-            {/*    )}*/}
-            {/*  </Field>*/}
-            {/*</div>*/}
-
-            {/*<div>*/}
-            {/*  <FormikInput variant={"addProduct"} icon={productName} type={"text"} name={"productName"}*/}
-            {/*               placeHolder={"نام کالا"}/>*/}
-            {/*  <ErrorMessage name="productName" component="div" className={"text-red-500 text-xs mt-2"}/>*/}
-            {/*</div>*/}
-
-            {/*<div>*/}
-            {/*  <Field*/}
-            {/*    className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}*/}
-            {/*    as={"select"} name={"categoryId"}>*/}
-            {/*    {[*/}
-            {/*      <option key="0" value="">*/}
-            {/*        دسته بندی </option>,*/}
-            {/*      fetchData*/}
-            {/*        .filter(subCategory => subCategory.id)*/}
-            {/*        .map(subCategory => (*/}
-            {/*          <option key={subCategory.id} value={subCategory.id}>*/}
-            {/*            {subCategory.name}*/}
-            {/*          </option>*/}
-            {/*        ))*/}
-            {/*    ]}*/}
-            {/*  </Field>*/}
-            {/*  <ErrorMessage name="categoryId" component="div" className={"text-red-500 text-xs mt-2"}/>*/}
-            {/*</div>*/}
-
-            {/*<div>*/}
-            {/*  <Field name="description">*/}
-            {/*    {({ field}:TextareaFieldProps) => (*/}
-            {/*      <textarea*/}
-            {/*        {...field}*/}
-            {/*        className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"*/}
-            {/*        placeholder="توضیحات"*/}
-            {/*      />*/}
-            {/*    )}*/}
-            {/*  </Field>*/}
-            {/*  <ErrorMessage name="description" component="div" className="text-red-500 text-xs" />*/}
-            {/*</div>*/}
-
-            {/*<div className={"flex gap-2"}>*/}
-            {/*  <div className={"w-[20rem]"}>*/}
-            {/*    <Button onClick={handleClose} type={"button"} classes={"bg-red-500 w-full flex justify-center"}*/}
-            {/*            variant={"common"}>لغو</Button>*/}
-            {/*  </div>*/}
-            {/*  <div className={"w-[20rem]"}>*/}
-            {/*    <Button type={"submit"} classes={"bg-blue-500 w-full flex justify-center"}*/}
-            {/*            variant={"common"}>ذخیره</Button>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-
-            {/*</div>*/}
-            {/*</Form>*/}
-            {/*)}*/}
-            {/*</Formik>*/}
-
-            <Formik initialValues={{productName: "", email: "", productImage: "", categoryId: '', description: ''}} validate={values => {
-              const errors: ProductSaveError = {};
-              if (!formik.values.productImage)
-                errors.productImage = "هوووووووووووووووی";
-
-              if (!values.productName)
-                errors.productName = "چی میزنی؟";
-              else if (/^.{0,3}$/.test(values.productName))
-                errors.productName = "تو زندگیت سعی کن آدم باشی";
-
-              if (values.categoryId === "") {
-                errors.categoryId = "سلکت بار نرفت تو چشت؟";
-              }
-
-              if(!values.description)
-                errors.description = "بنویس چند تا جمله دیگه"
-              else if(/^.{0,20}$/.test(values.description))
-                errors.description = "۲۰ تا کاراکتر بنویس انصافا"
-
-
-              return errors;
-            }}
                     onSubmit={(values, {setSubmitting}) => {
               setTimeout(() => {
                 console.log(values);
@@ -261,6 +102,7 @@ const AddProductModal = ({handleClose}) => {
                 });
                 setSubmitting(false);
               }, 400);
+
             }}>
               {() => (
                 <Form>
@@ -290,13 +132,11 @@ const AddProductModal = ({handleClose}) => {
                       </label>
                     </div>
                     <ErrorMessage name="productImage" component="div" className={"text-red-500 text-xs mt-2"}/>
-
                     <div>
                       <FormikInput variant={"addProduct"} icon={productName} type={"text"} name={"productName"}
                                    placeHolder={"نام کالا"}/>
                       <ErrorMessage name="productName" component="div" className={"text-red-500 text-xs mt-2"}/>
                     </div>
-
                     <div>
                       <Field
                         className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
@@ -315,16 +155,31 @@ const AddProductModal = ({handleClose}) => {
                       </Field>
                       <ErrorMessage name="categoryId" component="div" className={"text-red-500 text-xs mt-2"}/>
                     </div>
-
-
-
-                    <button type="submit">Submit</button>
+                    <div>
+                      <Field name="description">
+                        {({field}: TextareaFieldProps) => (
+                          <textarea
+                            {...field}
+                            className="block p-2.5 w-full h-36 resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="توضیحات"/>
+                        )}
+                      </Field>
+                      <ErrorMessage name="description" component="div" className="text-red-500 text-xs"/>
+                    </div>
+                    <div className={"flex gap-2"}>
+                      <div className={"w-[20rem]"}>
+                        <Button onClick={handleClose} type={"button"} classes={"bg-red-500 w-full flex justify-center"}
+                                variant={"common"}>لغو</Button>
+                      </div>
+                      <div className={"w-[20rem]"}>
+                        <Button type={"submit"} classes={"bg-blue-500 w-full flex justify-center"}
+                                variant={"common"}>ذخیره</Button>
+                      </div>
+                    </div>
                   </div>
                 </Form>
               )}
             </Formik>
-
-
           </div>
         </div>
       </div>
