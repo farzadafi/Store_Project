73f4d299918a6b5b915ff0c17b3f5d64bdb9dccb
farzadafi <farzadafi40@gmail.com>
2023-06-15 07:54:29 +0330

    add deleteProduct fucntion

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,59 +3,68 @@ import {LoginFormValue} from "@/interfaces";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080",
-})
+});
 
 class APIClient<T> {
   endPoint: string;
-  constructor(endPoint:string) {
-    this.endPoint = endPoint
+
+  constructor(endPoint: string) {
+    this.endPoint = endPoint;
   }
 
-  loginPost = (data:LoginFormValue) => {
+  loginPost = (data: LoginFormValue) => {
     return axiosInstance
       .post<T>(this.endPoint, data)
-      .then(result => result.data)
-  }
+      .then(result => result.data);
+  };
 
   getAllProduct = () => {
     return axiosInstance
       .get<T>(this.endPoint)
-      .then(result => result.data)
-  }
+      .then(result => result.data);
+  };
 
   getAllOrders = (cookie: string) => {
     const options = {
-      headers: {'Authorization': 'Bearer ' + cookie}
+      headers: {"Authorization": "Bearer " + cookie}
     };
     return axiosInstance
       .get<T>(this.endPoint, options)
-      .then(result => result.data)
-  }
+      .then(result => result.data);
+  };
 
   verifyToken = (cookie: string) => {
     const options = {
-      headers: {'Authorization': 'Bearer ' + cookie}
+      headers: {"Authorization": "Bearer " + cookie}
     };
     return axiosInstance
       .get<T>(this.endPoint, options)
-      .then(result => result.data)
-  }
+      .then(result => result.data);
+  };
 
   getAllSubCategory = () => {
     return axiosInstance
       .get<T>(this.endPoint)
-      .then(result => result.data)
-  }
+      .then(result => result.data);
+  };
 
   addProduct = (cookie: string, data: FormData) => {
     const options = {
-      headers: {'Authorization': 'Bearer ' + cookie}
+      headers: {"Authorization": "Bearer " + cookie}
     };
     return axiosInstance
-      .post<T>(this.endPoint,data, options)
-      .then(result => result.data)
-  }
-}
+      .post<T>(this.endPoint, data, options)
+      .then(result => result.data);
+  };
 
+  deleteProduct = (cookie: string, id: string) => {
+    const options = {
+      headers: {"Authorization": "Bearer " + cookie}
+    };
+    return axiosInstance
+      .delete<T>(`${this.endPoint}?id=${id}`, options)
+      .then(result => result.data);
+  };
+}
 
 export default APIClient;
