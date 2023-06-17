ceb7952704485cc416723559b7ec600ad945fe2b
farzadafi <farzadafi40@gmail.com>
2023-06-17 09:38:46 +0330

    export EditablePrice function

--- src/component/index.ts
+++ src/component/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from './input'
 export { default as FormikInput } from './formikInput'
 export { default as Card } from './card'
 export * from "./modal";
+export { default as EditablePrice } from './editablePrice'
