3d355e1a1a897edcead5d1c0a1abb44f3f88ffaf
farzadafi <farzadafi40@gmail.com>
2023-06-19 13:27:30 +0330

    add queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,16 @@ import {RouterProvider} from "react-router";
 import router from "@/routes";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-    <ToastContainer/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+      <ToastContainer/>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
