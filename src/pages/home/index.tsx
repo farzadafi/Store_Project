32b879c7cf18aee78696ae9256087bb7a89a3de5
farzadafi <farzadafi40@gmail.com>
2023-06-25 19:45:44 +0330

    add a new useEffect for navigate to another page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import {Footer, NormalHeader} from "@/layout";
-import {Outlet} from "react-router";
+import {Outlet, useNavigate} from "react-router";
 import {Container} from "@/containers";
+import {useEffect} from "react";
 
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    navigate("/main-products");
+  }, []);
 
   return (
     <Container>
