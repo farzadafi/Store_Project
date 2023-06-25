767cd57ee3aad2a470ad8cd56ea0dfea92bf2317
farzadafi <farzadafi40@gmail.com>
2023-06-25 15:54:24 +0330

    add sighOut funciton

--- src/layout/header/managerHeader/index.tsx
+++ src/layout/header/managerHeader/index.tsx
@@ -1,42 +1,65 @@
 import {Button} from "@/component";
 import {useLocation, useNavigate} from "react-router";
+import {useCookies} from "react-cookie";
+import {toast} from "react-toastify";
 
 const ManagerHeader = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [_cookies, _setCookie, removeCookie] = useCookies(["token"]);
 
   const currentRoute = location.pathname;
 
   const inventoryProductsHandle = () => {
-    navigate('inventory')
-  }
+    navigate("inventory");
+  };
 
   const productsHandle = () => {
-    navigate('products')
-  }
+    navigate("products");
+  };
 
   const ordersHandle = () => {
-    navigate('orders')
-  }
+    navigate("orders");
+  };
 
   const homeHandle = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
+
+  const showSuccessfulToastMessage = () => {
+    toast.success("ابلفضل نگهدارت :)", {
+      position: toast.POSITION.TOP_CENTER,
+      className: "toast-message"
+    });
+  };
+
+  const signOut = () => {
+    removeCookie("token");
+    showSuccessfulToastMessage();
+    navigate("/");
+  };
 
   return (
     <div className={"flex justify-between bg-[#8130d1] p-2 max-sm:gap-4 max-sm:flex-col max-sm:items-center"}>
       <div className={"flex items-center max-sm:justify-center"}>
-        <p className={"text-xs text-white max-sm:text-xl max-sm:whitespace-nowrap max-sm:text-center"}>پنل مدیریت فروشگاه</p>
+        <p className={"text-xs text-white max-sm:text-xl max-sm:whitespace-nowrap max-sm:text-center"}>پنل مدیریت
+          فروشگاه</p>
       </div>
       <div className={"flex gap-4 text-xs max-[550px]:flex-col"}>
         <div className={"flex gap-2"}>
-          <Button onClick={productsHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === "/main-manager-page/products" ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>تمامی کالا ها</Button>
-          <Button onClick={inventoryProductsHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === '/main-manager-page/inventory' ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>موجودی و قیمت ها</Button>
+          <Button onClick={productsHandle}
+                  classes={`max-sm:whitespace-nowrap ${currentRoute === "/main-manager-page/products" ? "bg-green-500" : "bg-[#569aff]"}`}
+                  variant={"managerButton"}>تمامی کالا ها</Button>
+          <Button onClick={inventoryProductsHandle}
+                  classes={`max-sm:whitespace-nowrap ${currentRoute === "/main-manager-page/inventory" ? "bg-green-500" : "bg-[#569aff]"}`}
+                  variant={"managerButton"}>موجودی و قیمت ها</Button>
         </div>
         <div className={"flex gap-2"}>
-          <Button onClick={ordersHandle} classes={`max-sm:whitespace-nowrap ${currentRoute === '/main-manager-page/orders' ? "bg-green-500" : "bg-[#569aff]"}`} variant={"managerButton"}>سفارش ها</Button>
+          <Button onClick={ordersHandle}
+                  classes={`max-sm:whitespace-nowrap ${currentRoute === "/main-manager-page/orders" ? "bg-green-500" : "bg-[#569aff]"}`}
+                  variant={"managerButton"}>سفارش ها</Button>
           <Button onClick={homeHandle} variant={"managerButton"}>صفحه اول</Button>
-          <Button onClick={homeHandle} variant={"managerButton"}>خروج</Button>
+          <Button onClick={signOut} variant={"managerButton"}>خروج</Button>
         </div>
       </div>
     </div>
