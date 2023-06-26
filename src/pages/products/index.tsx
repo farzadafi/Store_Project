fca529f68c085e6f5083ac72f46825a0e7ab62af
farzadafi <farzadafi40@gmail.com>
2023-06-26 10:13:16 +0330

    fix some classes

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -62,13 +62,13 @@ const Products = () => {
   function renderProductList([subCategoryName, products]: [string, Product[]], index: number, _array: [string, Product[]][]) {
     return (
       <div key={index} className={"mt-6 text-white"}>
-        <div className={"flex gap-12 flex-wrap pt-4 justify-center border m-2 rounded-[3rem] flex-col"}>
-          <h1 className={"text-center"}>{subCategoryName}</h1>
-          <div className={"flex flex-wrap justify-center gap-12"}>
+        <div className={"flex gap-4 flex-wrap pt-2 justify-center border m-2 rounded-[3rem] flex-col"}>
+          <h1 className={"text-center text-[2rem]"}>{subCategoryName}</h1>
+          <div className={"flex flex-wrap justify-center gap-12 p-4"}>
             {products.map(product => (
               <div key={product.id}
-                   className={"w-max flex flex-col mb-4 justify-center items-center gap-2 border p-4 rounded-[3rem]"}>
-                <img className="w-44 h-44 rounded-[3rem] max-sm:w-8 max-sm:h-8 flex-shrink-0 mx-auto"
+                   className={"w-max flex flex-col justify-center items-center gap-2 border p-4 rounded-[3rem]"}>
+                <img className="w-36 h-36 rounded-[3rem] max-sm:w-8 max-sm:h-8 flex-shrink-0 mx-auto"
                      src={"data:image/png;base64," + product.image} alt={product.name}/>
                 <div>
                   <p>{product.name}</p>
